36dc97a4e6e0ead6c826a2da903d370198e6abbd
varg90 <kuhn.alexey@gmail.com>
2026-04-09 19:40:07 +0300

    fix: editor button 32px to match summary height

--- ui/settings_window.py
+++ ui/settings_window.py
@@ -120,7 +120,7 @@ class SettingsWindow(QMainWindow, SnapMixin):
         self._quick_btn.clicked.connect(lambda: self._set_timer_mode("quick"))
 
         self._add_btn = make_icon_btn(Icons.PLUS, self.theme.text_hint,
-                                      size=S.ICON_HEADER, tooltip="Add files")
+                                      size=32, tooltip="Add files")
         self._add_btn.clicked.connect(self._open_editor)
 
         mode_row.addWidget(self._class_btn, 1)
