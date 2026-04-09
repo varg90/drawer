bcf1032e98573d593686b6a0f12d8416114618f5
varg90 <kuhn.alexey@gmail.com>
2026-04-09 19:14:29 +0300

    fix: revert icon oversize, bump close/minimize to 17px for visual balance

--- ui/widgets.py
+++ ui/widgets.py
@@ -9,10 +9,9 @@ from ui.icons import Icons
 def make_icon_btn(icon_name, color, size=S.ICON_HEADER, tooltip=""):
     """Small icon button with no background/border."""
     btn = QPushButton()
-    render_sz = int(size * 1.2)
     btn.setIcon(qta.icon(icon_name, color=color))
-    btn.setIconSize(QSize(render_sz, render_sz))
-    btn.setFixedSize(render_sz, render_sz)
+    btn.setIconSize(QSize(size, size))
+    btn.setFixedSize(size, size)
     btn.setCursor(Qt.CursorShape.PointingHandCursor)
     btn.setStyleSheet("background: transparent; border: none; padding: 0px;")
     if tooltip:
@@ -41,10 +40,9 @@ def make_icon_toggle(icon_on, icon_off, is_on, theme, size=S.ICON_HEADER):
     btn = QPushButton()
     icon_name = icon_on if is_on else icon_off
     color = theme.accent if is_on else theme.text_hint
-    render_sz = int(size * 1.2)
     btn.setIcon(qta.icon(icon_name, color=color))
-    btn.setIconSize(QSize(render_sz, render_sz))
-    btn.setFixedSize(render_sz, render_sz)
+    btn.setIconSize(QSize(size, size))
+    btn.setFixedSize(size, size)
     btn.setCursor(Qt.CursorShape.PointingHandCursor)
     btn.setStyleSheet("background: transparent; border: none; padding: 0px;")
     return btn
